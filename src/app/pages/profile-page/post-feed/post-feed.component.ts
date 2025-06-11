115c8f767d54d5e3bfaed41d725b7609b308ae86
victor-work <vmenzhik132f@gmail.com>
2025-06-11 18:33:36 +0500

    posts

--- src/app/pages/profile-page/post-feed/post-feed.component.ts
+++ src/app/pages/profile-page/post-feed/post-feed.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, inject, Renderer2} from '@angular/core';
 import {PostInputComponent} from '../post-input/post-input.component';
 import {PostComponent} from '../post/post.component';
+import {PostService} from '../../../data/services/post.service';
+import {firstValueFrom} from 'rxjs';
 
 @Component({
   selector: 'app-post-feed',
@@ -12,6 +14,31 @@ import {PostComponent} from '../post/post.component';
   templateUrl: './post-feed.component.html',
   styleUrl: './post-feed.component.scss'
 })
-export class PostFeedComponent {
+export class PostFeedComponent implements AfterViewInit {
+  postService = inject(PostService);
+  feed = this.postService.posts
 
+  hostElement = inject(ElementRef)
+  r2 = inject(Renderer2)
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.resizeFeed()
+  }
+
+  constructor() {
+    firstValueFrom(
+      this.postService.fetchPost()
+    )
+  }
+
+  ngAfterViewInit() {
+    this.resizeFeed()
+  }
+
+  resizeFeed() {
+    const {top} = this.hostElement.nativeElement.getBoundingClientRect();
+    const height = window.innerHeight - top - 24 - 24
+    this.r2.setStyle(this.hostElement.nativeElement, 'height', `${height}px`);
+  }
 }
